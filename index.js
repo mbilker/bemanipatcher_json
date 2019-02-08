3882fa3ce8fa7be4d9e9fb8c8f356b0472975b37
Matt Bilker <me@mbilker.us>
2019-02-08 01:05:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const vm = require('vm');
+
+const jsdom = require('jsdom');
+
+const html_files = fs.readdirSync('bemanipatcher').filter((file) => {
+  return file.endsWith(".html") && file !== "index.html";
+});
+
+const parent = [];
+
+function parse_page(file) {
+  const file_path = path.join('bemanipatcher', file);
+  const contents = fs.readFileSync(file_path, "utf-8");
+  const dom = new jsdom.JSDOM(contents);
+
+  const patch_sets = [];
+
+  const sandbox = {
+    window: {
+      addEventListener(name, fn) {
+        if (name === 'load') {
+          fn();
+        }
+      }
+    },
+    DllPatcherContainer: function DllPatcherContainer() {},
+    DllPatcher: function DllPatcher(dll_name, patches, description) {
+      patch_sets.push({ dll_name, description, patches });
+    },
+  };
+  vm.createContext(sandbox);
+
+  const document = dom.window.document.querySelectorAll('script');
+  document.forEach((elem) => {
+    const code = elem.innerHTML;
+    if (code.length > 0) {
+      vm.runInContext(code, sandbox);
+    }
+  });
+
+  parent.push({
+    file,
+    patch_sets,
+  });
+}
+
+for (const file of html_files) {
+  console.log(file);
+  parse_page(file);
+}
+
+const json = JSON.stringify(parent, null, 2);
+fs.writeFileSync('hex_edits.json', json);
+console.log('Wrote hex edits to hex_edits.json');
